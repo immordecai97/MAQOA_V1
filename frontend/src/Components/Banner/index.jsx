7e31c66d71b0a542ebe45b4047853d25ba07c225
Bubazeta <hector.dtapia@davinci.edu.ar>
2024-07-03 08:38:39 -0300

    feat: Banner component

---
+++ frontend/src/Components/Banner/index.jsx
@@ -0,0 +1,28 @@
+import "./style.css"
+const Banner = () => {
+  return (
+    <div className="banner h-[25rem] w-full flex   p-3 m-3 shadow-top-bottom">
+      <section className=" h-full flex items-center justify-center">
+        <div className="flex flex-col  w-[800px] p-3 gap-4">
+          <h2 className="text-5xl font-bold">
+            Download Our Mobile <span>App</span> !
+          </h2>
+          <p className="text-lg">Experience our website on the go with our mobile app. <br /> Browse, shop, and stay updated effortlessly. <br /> Download now and take best of our site with you!</p>
+          <div className="flex gap-3">
+            <button className="bg-black text-white py-2 px-4 rounded border border-transparent hover:bg-white hover:border-black hover:text-black transition " >App store</button>
+            <button className="bg-black text-white py-2 px-4 rounded border border-transparent hover:bg-white hover:border-black hover:text-black transition ">Play store</button>
+          </div>
+        </div>
+        <figure className="flex w-[17%] overflow-auto">
+          <img
+            className="w-full w-max-[370px] "
+            src="./../../public/images/phone-modificado-2.png"
+            alt=""
+          />
+        </figure>
+      </section>
+    </div>
+  );
+};
+
+export default Banner;
--- frontend/src/Pages/Home/index.jsx
+++ frontend/src/Pages/Home/index.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 import { ShopMaqoaContext } from './../../Context'
+import Banner from "../../Components/Banner"
 
 const Home = () => {
 	// const context = useContext(ShopMaqoaContext)
@@ -11,9 +12,10 @@ const Home = () => {
 			{/* TÍTULO */}
 			<h1 className="text-4xl font-bold mb-8">Welcome to MAQOA</h1>
 			{/* BANNER */}
-			<div className="bg-black w-full h-[20rem] flex justify-center items-center mb-8">
+			{/* <div className="bg-black w-full h-[20rem] flex justify-center items-center mb-8">
 				<h2 className="text-white">BANNER</h2>
-			</div>
+			</div> */}
+			<Banner/>
 			{/* PRODUCTOS */}
 			<ul className="flex flex-wrap gap-4 px-16 justify-center mt-6">
 				{
