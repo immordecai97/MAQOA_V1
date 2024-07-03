73f7aff3ca21d836f420be83d0ed2d52bd07a418
ImMordecai <carlosscripto@gmail.com>
2024-07-03 06:43:38 -0500

    Update index.jsx

--- frontend/src/Components/Banner/index.jsx
+++ frontend/src/Components/Banner/index.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
     <div className="banner h-[25rem] w-full flex   p-3 m-3 shadow-top-bottom">
       <section className=" h-full flex items-center justify-center">
         <div className="flex flex-col  w-[800px] p-3 gap-4">
-          <h2 className="text-5xl font-bold">
+          <h2 className="text-5xl font-extrabold">
             Download Our Mobile <span>App</span> !
           </h2>
           <p className="text-lg">Experience our website on the go with our mobile app. <br /> Browse, shop, and stay updated effortlessly. <br /> Download now and take best of our site with you!</p>
