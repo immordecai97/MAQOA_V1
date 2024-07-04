0a0b255c3ee36830eb988df67a1e13f604263229
Bubazeta <hector.dtapia@davinci.edu.ar>
2024-07-04 08:02:28 -0300

    feat: maketado store view

--- frontend/src/Pages/Store/index.jsx
+++ frontend/src/Pages/Store/index.jsx
@@ -16,7 +16,7 @@ const Store = () => {
       </div>
       <div className="flex items-start gap-4 w-full max-w-[900px]">
         <div className="relative w-[6rem] h-[6rem]">
-            <figure className="absolute -top-12 -left-40 shadow-top-bottom border bg-white border-red-700 w-[15rem] h-[15rem] rounded-full overflow-hidden flex items-center justify-start">
+            <figure className="absolute -top-12 -left-40 shadow-top-bottom bg-white w-[15rem] h-[15rem] rounded-full overflow-hidden flex items-center justify-start">
             <img  className="w-full object-cover " src="https://i.pinimg.com/564x/d8/d3/46/d8d34638bc587b2dbc279ecffd4acc12.jpg" alt=""/>
             </figure>
         </div>
@@ -29,6 +29,20 @@ const Store = () => {
             voluptas soluta!
           </p>
         </div>
+      </div>
+        <h2 className="mt-24 font-extrabold text-[2.1rem]">Catalogue</h2>
+      <div className="mt-16 text-center w-[250px] ">
+        <figure className="w-full border rounded">
+            <img className="w-full max-w-[250px] rounded-sm" src="https://i.pinimg.com/736x/1e/da/07/1eda0712f753729ad90193ce2a138f0e.jpg" alt="" />
+            <figcaption className="font-bold mt-1" >Figcaption de la foto</figcaption>
+            <p className="text-start p-2">categori: saraza</p>
+            <h3 className="text-start px-2 font-bold">titulo</h3>
+            <p className="text-start px-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> In tenetur accusamus facilis reiciendis sunt vitae autem repellat quidem, hic commodi! Iusto, sit voluptatibus?</p>
+            <div className="flex gap-3 justify-center mt-2">
+            <button className="mb-2 w-full max-w-[60px] p-1 bg-black rounded text-white transition  self-end border border-transparent hover:bg-white hover:border-black hover:text-black">Button</button>
+            <button className="mb-2 w-full max-w-[60px] p-1 bg-black rounded text-white transition  self-end border border-transparent hover:bg-white hover:border-black hover:text-black">🧡</button>
+            </div>
+        </figure>
       </div>
     </LayoutStore>
   );
