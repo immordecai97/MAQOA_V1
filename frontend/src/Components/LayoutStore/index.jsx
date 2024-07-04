9984ecbed52490aca9ec94b679319dcf7e8b77d9
Bubazeta <hector.dtapia@davinci.edu.ar>
2024-07-04 05:59:42 -0300

    div

--- frontend/src/Components/LayoutStore/index.jsx
+++ frontend/src/Components/LayoutStore/index.jsx
@@ -1,6 +1,6 @@
 const LayoutStore = ({ children }) => {
 	return (
-		<div className='flex flex-col items-center mt-20'>
+		<div className='flex flex-col items-center mt-16'>
 			{children}
 		</div>
 	)
--- frontend/src/Pages/Store/index.jsx
+++ frontend/src/Pages/Store/index.jsx
@@ -12,24 +12,24 @@ const Store = () => {
               className="w-full max-w-[450px]"
             />
           </figure>
-          <figure className="absolute top-64 shadow-top-bottom left-24 border bg-white border-red-700 w-[15rem] h-[15rem] rounded-full overflow-hidden flex items-center justify-start">
-            <img
-              src="https://i.pinimg.com/564x/d8/d3/46/d8d34638bc587b2dbc279ecffd4acc12.jpg"
-              alt=""
-              className="w-full object-cover "
-            />
-          </figure>
         </div>
       </div>
-          <div className=" ">
-            <h1 className="font-bold text-[2.2rem]">Pizza Bay</h1>
-            <p className="w-full max-w-[600px]">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-              ab, vero consequuntur praesentium possimus officia molestias, quod
-              earum sunt eligendi adipisci a quaerat iste enim non aspernatur in
-              voluptas soluta!
-            </p>
-          </div>
+      <div className="flex items-start gap-4 w-full max-w-[900px]">
+        <div className="relative w-[10rem] h-[10rem]">
+            <figure className="z-10 absolute -top-12 -left-40 shadow-top-bottom border bg-white border-red-700 w-[15rem] h-[15rem] rounded-full overflow-hidden flex items-center justify-start">
+            <img  className="w-full object-cover " src="https://i.pinimg.com/564x/d8/d3/46/d8d34638bc587b2dbc279ecffd4acc12.jpg" alt=""/>
+            </figure>
+        </div>
+        <div>
+          <h1 className="font-bold text-[2.2rem]">Pizza Bay</h1>
+          <p className="w-full max-w-[600px]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ab,
+            vero consequuntur praesentium possimus officia molestias, quod earum
+            sunt eligendi adipisci a quaerat iste enim non aspernatur in
+            voluptas soluta!
+          </p>
+        </div>
+      </div>
     </LayoutStore>
   );
 };
