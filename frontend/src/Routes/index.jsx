189e70f9d40351fa2eefa6c07cdd28745ae986c5
Bubazeta <hector.dtapia@davinci.edu.ar>
2024-07-05 03:05:11 -0300

    aboutus

--- frontend/src/Routes/index.jsx
+++ frontend/src/Routes/index.jsx
@@ -4,6 +4,7 @@ import ProductDetails from '../Pages/ProductDetails'
 import Store from '../Pages/Store'
 import Login from '../Pages/Login'
 import Register from '../Pages/Register'
+import AboutUs from '../Pages/AboutUs'
 
 /**Creamos el array de rutas */
 const AppRoutes = () => {
@@ -11,6 +12,7 @@ const AppRoutes = () => {
 		{ path: '/', element: <Home /> },
 		{ path: '/login', element: <Login /> },
 		{ path: '/register', element: <Register /> },
+		{ path: '/aboutus', element: <AboutUs /> },
 		{ path: '/store/:id', element: <Store /> },
 		{ path: '/product/:id', element: <ProductDetails /> },
 	])
